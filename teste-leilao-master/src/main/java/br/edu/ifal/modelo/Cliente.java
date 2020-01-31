eb5e8a459ce7138c5b9180a60f1665f1ab268305
clariceavlis <56318018+clariceavlis@users.noreply.github.com>
2020-01-31 11:31:51 -0300

    Add files via upload

---
+++ teste-leilao-master/src/main/java/br/edu/ifal/modelo/Cliente.java
@@ -0,0 +1,47 @@
+package br.edu.ifal.modelo;
+
+public class Cliente {
+     private int id;
+     private String nome;
+
+    public Cliente(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id != other.id)
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+   
+}
---
+++ teste-leilao-master/src/test/java/br/edu/ifal/controle/ControleLeilaoTest.java
@@ -0,0 +1,113 @@
+package br.edu.ifal.controle;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.edu.ifal.modelo.Cliente;
+import br.edu.ifal.modelo.Lance;
+import br.edu.ifal.modelo.Leilao;
+import br.edu.ifal.modelo.Produto;
+
+public class ControleLeilaoTest {
+
+    Produto produto;
+    double valorMinimo;
+    Leilao leilao;
+    Cliente cliente;
+    Cliente cliente2;
+    ControleLeilao controle;
+
+    @Before
+    public void inicializacao(){
+        produto = new Produto("PS4", "video-game");
+        valorMinimo = 2500;
+        leilao = new Leilao(produto, valorMinimo);
+        cliente = new Cliente(1, "Ana");
+        cliente2 = new Cliente(2, "Zé");
+        controle = new ControleLeilao();
+    }
+
+
+    @Test
+    public void deveRetornarVerdadeiroParaUmLanceMaiorQueOValorMinimoDoProduto() {
+        Lance novoLance = new Lance(cliente, 2501);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = true;
+        assertEquals(validadeEsperada, validadeRetornada);
+    }
+    @Test
+    public void deveRetornarFalsoParaUmLanceMenorQueOValorMinimoDoProduto() {
+        Lance novoLance = new Lance(cliente, 2000);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = false;
+        assertEquals(validadeEsperada, validadeRetornada);
+    }
+    @Test
+    public void deveRetornarVerdadeiroParaUmLanceIgualAoValorMinimoDoProduto() {
+        Lance novoLance = new Lance(cliente, 2500);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = true;
+        assertEquals(validadeEsperada, validadeRetornada);
+    }
+
+    @Test
+    public void deveRetornarFalsoParaUmLanceDadoPeloMesmoClienteDoUltimoLance(){
+        leilao.adicionarLance(new Lance(cliente, 2600));
+        Lance novoLance = new Lance(cliente, 2700);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = false;
+        assertEquals(validadeEsperada, validadeRetornada);
+    }
+
+    @Test
+    public void deveRetornarVerdadeiroParaUmLanceDadoPorUmClienteDiferenteDoClienteDoUltimoLance(){
+        leilao.adicionarLance(new Lance(cliente, 2600));
+        Lance novoLance = new Lance(cliente2, 2700);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = true;
+        assertEquals(validadeEsperada, validadeRetornada);
+    }
+    
+    @Test
+    public void deveRetornarVerdadeiroParaOPrimeiroLanceDoLeilaoMaiorQueOValorMinimo(){
+        Lance novoLance = new Lance(cliente, 2700);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = true;
+        assertEquals(validadeEsperada, validadeRetornada);
+
+        
+    }
+
+    @Test
+    public void deveRetornarFalsoParaUmNovoLanceMenorQueOLanceAnterior(){
+        leilao.adicionarLance(new Lance(cliente, 3000));
+        Lance novoLance = new Lance(cliente2, 2900);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = false;
+        assertEquals(validadeEsperada, validadeRetornada);
+
+    }
+
+    @Test
+    public void deveRetornarVerdadeiroParaUmLanceMaiorQueOLanceAnterior(){
+        leilao.adicionarLance (new Lance(cliente, 2600));
+        Lance novoLance = new Lance (cliente2, 2700);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = true;
+        assertEquals(validadeEsperada, validadeRetornada); 
+
+    }
+
+    @Test
+    public void deveRetornarFalsoParaUmNovoLanceIgualAoValorAnterior(){
+        leilao.adicionarLance (new Lance(cliente, 2600));
+        Lance novoLance = new Lance (cliente2, 2600);
+        boolean validadeRetornada = controle.validarLance(novoLance, leilao);
+        boolean validadeEsperada = false;
+        assertEquals(validadeEsperada, validadeRetornada); 
+
+    }
+
+}
